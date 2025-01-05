569a1a4686de17c8fbb3a708c4f586caff7fb467
Shubham Sahu <shubham.sahu.techie@gmail.com>
2025-01-05 17:15:59 +0530

    java

---
+++ number_in_java/number.java
@@ -0,0 +1,17 @@
+public class number {
+  public static void main(String[] args) {
+    
+
+    char ch = 'a';
+    System.out.println(ch);
+
+     int a = 15;
+     System.out.println(a);
+
+    /*
+     * 3
+     * 
+     */
+  }
+  
+}
