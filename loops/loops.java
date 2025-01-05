a538722de8a5bba99236765a5890c7ab961c1452
Shubham Sahu <shubham.sahu.techie@gmail.com>
2025-01-05 19:33:36 +0530

    loops in java

---
+++ loops/loops.java
@@ -0,0 +1,19 @@
+
+
+public class loops {
+
+public static void main(String[] args) {
+  
+
+  for (int i = 1; i <= 5; i++) {
+    System.out.println("Count: " + i);
+}
+
+
+int i = 1;
+while (i <= 5) {
+    System.out.println("Count: " + i);
+    i++;
+}
+}
+}
