a7e771e887b33e8c980162af866aaba9f922c819
Shubham Sahu <shubham.sahu.techie@gmail.com>
2025-01-06 18:16:42 +0530

    new loopp add

--- loops/loops.java
+++ loops/loops.java
@@ -14,6 +14,16 @@ int i = 1;
 while (i <= 5) {
     System.out.println("Count: " + i);
     i++;
+
+    int j = 1;
+    while (i <= 5) {
+        System.out.println("Count: " + i);
+        i++;
+
+
+
+
+
 }
 }
 }
