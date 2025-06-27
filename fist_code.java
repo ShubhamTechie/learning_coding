042bcdaa7099f3b223a1b1bbc85dd78cb2a4c2b3
Shubham Sahu <shubham.sahu.techie@gmail.com>
2025-06-28 00:44:42 +0530

    hello this is s demo code to learn git

---
+++ fist_code.java
@@ -0,0 +1,23 @@
+public class fist_code{
+
+
+
+
+public static void main(String[] args) {
+  
+
+System.out.println("hello world");
+
+int s = 10;
+int d = 20;
+
+int sum = s+d;
+
+
+System.out.println(sum);
+
+}
+
+
+
+}
---
+++ shub.java
@@ -0,0 +1,9 @@
+public class shub {
+
+
+
+  public static void main(String[] args) {
+    System.out.println("hi i am shubham, sahu");
+  }
+  
+}
