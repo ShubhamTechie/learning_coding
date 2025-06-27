68bb20c61986b9a6cf8d57c7d5d822aaa7547f3a
Shubham Sahu <shubham.sahu.techie@gmail.com>
2025-06-28 01:53:08 +0530

    add sum function

--- vcsdhgc.java
+++ vcsdhgc.java
@@ -1,8 +1,15 @@
+import input_in_java.input;
+
 public class vcsdhgc {
   public static void main(String[] args) {
     
 
     System.out.println("hello wocb");
+
+     int d = 55;
+     int s = 56;
+     int sum = d +s;
+
   }
   
 }
