75595e44b05cfd749d36174de21bdf69b1be6224
Shubham Sahu <shubham.sahu.techie@gmail.com>
2025-06-28 11:29:47 +0530

    Updated Main.java and added first_java.java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+        int a= 14;
+
+        int b=15;
+
+
+        int sum = a+b;
+
+
+
+    }
+}
