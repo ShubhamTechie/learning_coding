f55a08216cc6e3083a1abba53cffc1c00910fc13
Shubham Sahu <shubham.sahu.techie@gmail.com>
2025-06-28 11:14:30 +0530

    solid prin

---
+++ solid_principle/solid_prin.java
@@ -0,0 +1,26 @@
+package solid_principle;
+
+public class solid_prin {
+  
+
+
+  public static void main(String[] args) {
+    
+    /*
+     * solid prin are the rules and guidlines
+     * 1 single responsibility principle
+     * 2 open and close principle
+     * 3 liskov substituution principle
+     * 4 inface segregation
+     * 5 dependency inversion prin
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+
+
+
+  }
+}
