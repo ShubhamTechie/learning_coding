cd29efd60339e0fe7512e633d1b7a9389db552d0
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-02-28 16:35:59 +0530

    add new java code

---
+++ src/Variables.java
@@ -0,0 +1,30 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+
+
+        int a =1;
+ int b;
+
+
+boolean  isVeg = true;
+
+float myFlot = 5022f;
+
+
+long l = 8517555555554545454L;
+
+
+String s = "shubham";
+
+        System.out.println(a);
+        System.out.println(l);
+        System.out.println(isVeg);
+        System.out.println(myFlot);
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(a);
+
+    }
+}
