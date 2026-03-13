3610a2aec96dac71e17a31e0df7072e20cd73471
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-03-13 12:57:03 +0530

    if else

--- src/if_else.java
+++ src/if_else.java
@@ -1,18 +1,23 @@
+import java.util.Scanner;
+
 public class if_else {
 
     public static void main(String[] args) {
 
 
         System.out.println("if else statrmnrny");
+        Scanner sc = new Scanner(System.in);
 
-        String str = "true";
-        if (str == "shubham")
+        String str = sc.nextLine();
+        if (str.equals("shubham"))
         {
             System.out.println("true");
-        }else
+        }
+        else
         {
             System.out.println("false");
         }
 
     }
 }
+
