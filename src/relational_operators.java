ee3b3f9362178b7f74be4977e98f9dcd63868e66
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-03-13 15:24:23 +0530

    relation operators

---
+++ src/relational_operators.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class relational_operators {
+
+
+    public static void main(String[] args) {
+
+
+
+
+//        ==  comapare  it convert intoo boolean while comappe
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+
+        if (age >= 18){
+            System.out.println("adult eligible to drive");
+        }else {
+
+            System.out.println("not eligible");
+        }
+
+
+
+
+
+
+    }
+}
