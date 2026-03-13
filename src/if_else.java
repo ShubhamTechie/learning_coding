0388df8a9cb9e92c5b9609d0fa4059e695930efc
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-03-13 14:52:55 +0530

    if else find age

--- src/if_else.java
+++ src/if_else.java
@@ -4,29 +4,45 @@ public class if_else {
 
     public static void main(String[] args) {
 
+//
+//        System.out.println("if else statrmnrny");
+//        Scanner sc = new Scanner(System.in);  String str = sc.nextLine();
+//        if (str.equals("shubham"))
+//        {
+//            System.out.println("true");
+//        }
+//        else
+//        {
+//            System.out.println("false");
+//        }
+//        boolean isMale = false;
+//        String name = "shubham sahu";
+//        String s = "tru ";
+//
+//        if (isMale){
+//            System.out.println(s);
+//        }else {
+//
+//
+//            System.out.println( name);
+//            System.out.println(str);    }
 
-        System.out.println("if else statrmnrny");
-        Scanner sc = new Scanner(System.in);  String str = sc.nextLine();
-        if (str.equals("shubham"))
-        {
-            System.out.println("true");
-        }
-        else
-        {
-            System.out.println("false");
-        }
-        boolean isMale = false;
-        String name = "shubham sahu";
-        String s = "tru ";
 
-        if (isMale){
-            System.out.println(s);
+
+        boolean isSeniorCitizwn = false;
+        boolean isAnAdult = false;
+
+        if (isSeniorCitizwn){
+            System.out.println("hello senior citizen");
         }else {
+            if (isAnAdult) {
+                System.out.println("hello adult");
+
+            } else {
+                System.out.println("hello child");
+            }
+
+        }}}
 
 
-            System.out.println( name);
-            System.out.println(str);    }
-
-    }
-}
 
