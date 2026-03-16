19fbf4f29c4f76cc68894d16590514ce54129ab7
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-03-16 17:45:21 +0530

    print two numbrs

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,8 +1,18 @@
+import java.util.Arrays;
+
 public class ArrayList {
 
     public static void main(String[] args) {
 
 
         System.out.println("array list");
+
+
+
+
+        
+
+
+
     }
 }
---
+++ src/PrintToNumber.java
@@ -0,0 +1,14 @@
+public class PrintToNumber {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(printNumber(4,5));
+
+    }
+
+    public static int printNumber(int a , int b){
+        int sum = a + b;
+        return sum;
+    }
+}
