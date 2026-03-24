40c6f68b09dd109e91cefc690fab8d6f952b7fad
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-03-24 21:23:55 +0530

    2d array

--- src/ArrayUtility.java
+++ src/ArrayUtility.java
@@ -31,4 +31,33 @@ public class ArrayUtility {
         }
         System.out.println();
     }
+
+    public static int[][] input2DArray() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the number of ROWS");
+        int rows = input.nextInt();
+        System.out.println("enter the number of COLUMNS");
+        int column = input.nextInt();
+        int[][] myArr = new int[rows][column];
+
+        int i = 0;
+        while (i < rows) {
+            int j = 0;
+            while (j < column) {
+                System.out.print("please enter row " +
+                        (i + 1) + " , : " + (j + 1) + " ; ");
+                myArr[i][j] = input.nextInt();
+                j++;
+            }
+
+            i++;
+        }
+
+
+        return myArr;
+
+    }
+
+
 }
--- src/MergeSoretdArray.java
+++ src/MergeSoretdArray.java
@@ -10,9 +10,38 @@ public class MergeSoretdArray {
 
         int[] arr1 = ArrayUtility.inputArray();
         int[] arr2 = ArrayUtility.inputArray();
-        int[] mergeArr =merge(arr1,arr2);
+        int[] mergeArr = merge(arr1, arr2);
 
 
+        System.out.print("your merged araay ois  ");
+        ArrayUtility.displayArray( mergeArr);
+
+    }
+
+
+    public static int[] merge(int[] arr1, int[] arr2) {
+
+
+        int newSize = arr1.length + arr2.length;
+        int[] newArr = new int[newSize];
+
+        int i = 0, j = 0, k = 0;
+        while (i < arr1.length || j < arr2.length) {
+            if (j == arr2.length ||
+                    (i == arr2.length && arr1[i] < arr2[j])) {
+                newArr[k] = arr1[i];
+                i++;
+                k++;
+            } else {
+                newArr[k] = arr2[j];
+                j++;
+                k++;
+            }
+
+        }
+
+
+        return newArr;
     }
 
 
